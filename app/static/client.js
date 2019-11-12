e595d73a42c98c9b69f680e917a4d30d2e4e5d09
Vincent Palumbo <vinnypalumbo.com@gmail.com>
2019-11-12 09:57:13 -0500

    Fix Done! label not disappearing if uploading the same image

--- app/static/client.js
+++ app/static/client.js
@@ -2,11 +2,12 @@ var el = x => document.getElementById(x);
 
 function showPicker() {
   el("file-input").click();
+  el("result-label").innerHTML = ``;
 }
 
 function showPicked(input) {
   el("upload-label").innerHTML = input.files[0].name;
-  el("result-label").innerHTML = ``;
+  
   var reader = new FileReader();
   reader.onload = function(e) {
     el("image-picked").src = e.target.result;
