5f593e31d533fa674eaaec6f95a49c1dcd0a73cd
Vincent Palumbo <vinnypalumbo.com@gmail.com>
2019-11-14 17:11:23 -0500

    Serve the web app on host 0.0.0.0 for the render app

--- app/server.py
+++ app/server.py
@@ -44,4 +44,4 @@ async def analyze(request):
 
 if __name__ == '__main__':
     if 'serve' in sys.argv:
-        uvicorn.run(app=app, host='127.0.0.1', port=5000, log_level="info")
+        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
